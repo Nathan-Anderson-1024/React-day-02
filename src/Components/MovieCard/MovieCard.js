8792a6df5e43be56bbf669be3be7c1df650f3b29
Nathan Anderson's Laptop <nathan.anderson.1024@gmail.com>
2022-09-25 13:34:24 -0700

    added alt to img

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -5,10 +5,10 @@ import logo from './batman_v_superman.jpg';
 export default function MovieCard(props) {
   return (
     <div className='movie-container'>
-        <img src={logo} className='logo'></img>
+        <img src={logo} className='logo' alt='batman v superman poster'></img>
         <div className='title'>
             <h3>{props.movieObject.Title}</h3>
-            <a href={props.movieObject.Poster} target='_blank' className='button'>
+            <a href={props.movieObject.Poster} target='_blank' className='button' rel='noreferrer'>
               <button>{props.movieObject.Type}</button>
             </a>
             
